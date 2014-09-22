92fadea3cf3161856823f22fb0374485937ec2c3
Arron Gibson <github@infinitymedia.ca>
2014-09-22 18:13:00 -0400

    Adding typography sample

---
+++ wp-content/themes/wp-starter/includes/typography-sample.php
@@ -0,0 +1,176 @@
+	<!--begin primary content-->
+	<h1>Drop Cap</h1>
+	<p><span class="dropcap">P</span>ellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+	<h3 class="short_headline"><span>Blockquote</span></h3>
+	<blockquote>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
+	</blockquote>
+	<h3 class="short_headline"><span>Headlines</span></h3>
+	<h1>Header 1</h1>
+	<h2>Header 2</h2>
+	<h3>Header 3</h3>
+	<h4>Header 4</h4>
+	<h5>Header 5</h5>
+	<h6>Header 6</h6>
+	<h3 class="short_headline"><span>Paragraphs</span></h3>
+	<p> According to Wikipedia: a paragraph (from the Greek <a href="#">paragraphos</a> , "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. The start of a paragraph is indicated by beginning on a new line. Sometimes the first line is indented; sometimes it is indented without beginning a new line. </p>
+	<p> A paragraph typically consists of a unifying main point, thought, or idea accompanied by supporting details. The non-fiction paragraph usually begins with the general and moves towards the more specific so as to advance an argument or point of view. Each paragraph builds on what came before and lays the ground or run the length of multiple pages, and may consist of one or many sentences. When dialogue is being quoted in fiction, a new paragraph is used each time the person being quoted changed. </p>
+	<h3 class="short_headline"><span>Unordered List</span></h3>
+	<ul>
+		<li>Apple</li>
+		<li>Pear</li>
+		<li>Banana</li>
+		<li>Orange</li>
+		<li>Carrot</li>
+	</ul>
+	<h3 class="short_headline"><span>Ordered List</span></h3>
+	<ol>
+		<li>First</li>
+		<li>Second</li>
+		<li>Third</li>
+		<li>Fourth</li>
+	</ol>
+	<h3 class="short_headline"><span>Definition List</span></h3>
+	<dl>
+		<dt>Definition List Title</dt>
+		<dd>This is a definition list division.</dd>
+		<dt>Inkscape</dt>
+		<dd>Open Source vector graphics editor</dd>
+		<dt>SVG</dt>
+		<dd>Scalable Vector Graphics</dd>
+		<dd>W3C standard</dd>
+		<dt>GIMP</dt>
+		<dd>GNU Image Manipulation Program</dd>
+		<dt>Drupal</dt>
+		<dd>Content Management System</dd>
+	</dl>
+	<h3 class="short_headline"><span>Nested List</span></h3>
+	<ol>
+		<li> one, two
+			<ol>
+				<li>buckle my shoe</li>
+			</ol>
+		</li>
+		<li> three, four
+			<ol>
+				<li>knock at the door</li>
+			</ol>
+		</li>
+		<li> Five, six
+			<ol>
+				<li>pick up sticks</li>
+			</ol>
+		</li>
+		<li> Seven, eight, lay them straight
+			<ol>
+				<li>Nine, ten, a big fat hen</li>
+				<li>Eleven, twelve, dig and delve</li>
+				<li>Thirteen, fourteen, maids a’courting</li>
+				<li>Fifteen, sixteen, maids in the kitchen</li>
+				<li>Seventeen, eighteen, maids a’waiting</li>
+				<li>Nineteen, twenty, my platter’s empty</li>
+			</ol>
+		</li>
+	</ol>
+	<h3 class="short_headline"><span>Responsive Table</span></h3>
+	<div class="overthrow"><table class="table table-striped table-bordered responsive">
+		<thead>
+			<tr>
+				<th> Mauris </th>
+				<th> Tempor </th>
+				<th> Praesent lacus nulla </th>
+				<th> Quis </th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td> Mauris </td>
+				<td> 23.000 </td>
+				<td> Suspendisse viverra </td>
+				<td> Yes </td>
+			</tr>
+			<tr>
+				<td> Trupis </td>
+				<td> 1.000 </td>
+				<td> Placerat tortor aesent semper </td>
+				<td> No </td>
+			</tr>
+			<tr>
+				<td> Sagittis </td>
+				<td> 122.000 </td>
+				<td> Neque vel condimentum </td>
+				<td> No </td>
+			</tr>
+			<tr>
+				<td> Libero </td>
+				<td> 500 </td>
+				<td> Hendrerit, lectus elit pretium </td>
+				<td> Yes </td>
+			</tr>
+			<tr>
+				<td> Tristique </td>
+				<td> 100.000 </td>
+				<td> Ligula nec consequat </td>
+				<td> Yes </td>
+			</tr>
+			<tr>
+				<td> Id Neque </td>
+				<td> 600.000 </td>
+				<td> Etiam sodales orci nec </td>
+				<td> No </td>
+			</tr>
+		</tbody>
+	</table></div>
+	<h3 class="short_headline"><span>Pre-formatted Text</span></h3>
+	<pre><code>h4 { 
+		border:1px solid #ddd; 
+		margin: 1.5em 0; 
+		background:#f8f8f8;
+		white-space: pre;	
+		overflow: auto; /* responsive */
+		padding:1%;
+	}</code></pre>
+	<h3 class="short_headline"><span>Phrase Elements</span></h3>
+	<h5>Em</h5>
+	<p> Element used to <em>put emphasis on certain information</em>. Most browsers display emphased text in italics by default. </p>
+	<p> Sample: <em>This is emphasied text</em>. </p>
+	<h5>Strong</h5>
+	<p> This element stands for "stronger emphasis" and is used for marking more important text. </p>
+	<p> Sample: <strong>This is text with stronger emphasis</strong> </p>
+	<h5>Cite</h5>
+	<p> According to <cite>W3C specification</cite>, we use <code>cite</code> element to define source of a quotation or reference. </p>
+	<p> Sample: <cite>This is quotation source</cite> </p>
+	<h4>Dfn</h4>
+	<p> Element used to markup inline definition of a single term. </p>
+	<p> Sample: <dfn>This is definition</dfn> </p>
+	<h5>code</h5>
+	<p> This element informs the browser that it contains a computer code, such as XHTML markup. </p>
+	<p> Sample: <code>document.write("Hello world");</code> </p>
+	<h4>samp</h4>
+	<p> Defines computer output data, for example we can use it to markup error messages </p>
+	<p> Sample: <samp>Error: no such file or directory</samp> </p>
+	<h5>kbd</h5>
+	<p> Means information that should be entered by the user. </p>
+	<p> Sample: press <kbd>Alt + F4</kbd> to close this window. <kbd>ESC</kbd> </p>
+	<h4>var</h4>
+	<p> Means variables used in computer programs or scripts. </p>
+	<p> Sample: <var>counter</var> </p>
+	<h5>abbr</h5>
+	<p> Abbreviation / a shortened form of a word or phrase </p>
+	<p> Sample: <abbr title="doctor">dr</abbr> </p>
+	<h5>q: inline quotation</h5>
+	<p>Sample: <q>This is a sample inline quotation</q></p>
+	<h5>Other elements</h5>
+	<h6>Subscript and superscript samples</h6>
+	<p> H<sub>2</sub>O, </p>
+	<p> E = mc<sup>2</sup>, </p>
+	<p> The 14<sup>th</sup> of September </p>
+	<h5>Insertion and deletion samples</h5>
+	<p> <ins datetime="2009-07-12T08:15:30+02:00" title="Added new information according to suggestions">This information was just added.</ins><br>
+		<del>While this information is no longer valid.</del> </p>
+		<h5>Presentational elements</h5>
+		<p> <strong>Bold text</strong><br>
+			<em>Italic text</em><br>
+			<small>Small</small> </p>
+			<h5> Horizontal rule: </h5>
+			<hr>
